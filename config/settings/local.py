c5454c633c5a0060311fc2caca05c9524c1b27b4
thejuni96 <thejuni96@gmail.com>
2023-05-05 11:32:12 +0500

    Defined project layout. (inspired by cookiecutter django)

---
+++ config/settings/local.py
@@ -0,0 +1,6 @@
+from .base import *  # noqa
+from .base import env
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
---
+++ config/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for goal_maven project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(BASE_DIR / "goal_maven3"))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+
+application = get_wsgi_application()
