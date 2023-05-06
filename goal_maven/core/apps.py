16ce2822675e03154db51744a465408d10454cf8
thejuni96 <thejuni96@gmail.com>
2023-05-07 04:22:28 +0500

    Configured docker-compose and github actions to run wait_for_db command.

---
+++ goal_maven/core/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+# from django.utils.translation import gettext_lazy as _
+
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'goal_maven.core'
+    # verbose_name = _("Users")
