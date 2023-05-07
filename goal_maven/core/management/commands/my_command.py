a3912f0f6661ff0e09b761be15ee3a50015f2b00
thejuni96 <thejuni96@gmail.com>
2023-05-07 17:01:36 +0500

    Configured custom User model for Goal-Maven.

---
+++ goal_maven/core/management/commands/my_command.py
@@ -0,0 +1,26 @@
+"""
+Django test command.
+"""
+import os
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to test anything."""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write(self.style.SUCCESS(os.getcwd()))
+
+        # self.stdout.write('Waiting for database...')
+        # db_up = False
+        # while db_up is False:
+        #     try:
+        #         self.check(databases=['default'])
+        #         db_up = True
+        #     except (Psycopg2OpError, OperationalError):
+        #         self.stdout.write('Database unavailable, waiting 1 second...')
+        #         time.sleep(1)
+
+        # self.stdout.write(self.style.SUCCESS('Database available!'))
